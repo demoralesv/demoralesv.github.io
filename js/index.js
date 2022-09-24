afc5bb15d32ffc3460003a8347aea31b18d056dc
steven15HC <steven99854@gmail.com>
2022-09-24 13:25:19 -0600

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,11 @@
 window.onload = async () => {
   console.log("ahora si");
   document.getElementById("options").focus();
+  const form = document.getElementById("options");
+
+  form.addEventListener("focusout", (event) => {
+    document.getElementById("options").focus();
+  });
 };
 
 const preventFocus = (event) => {
