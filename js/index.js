b25faa7b9a6a4020cc47d7d4ad417500e8041142
steven15HC <steven99854@gmail.com>
2022-09-26 17:59:59 -0600

    Update index.js

--- js/index.js
+++ js/index.js
@@ -14,19 +14,23 @@ const preventFocus = (event) => {
   document.getElementById("options").focus();
 };
 
-const enterCommand = (event) =>{
-  if(event.keyCode === 13){
+const enterCommand = (event) => {
+  if (event.keyCode === 13) {
     event.preventDefault(); // Ensure it is only this code that runs
     command.push(document.getElementById("options").value);
     document.getElementById("options").value = "";
-    if(command?.length === 5){
-      command.filter((co,index)=>{
-        return index !== 2 && co
-      })
+    if (command?.length === 5) {
+      command.filter((co, index) => {
+        return index !== 2 && co;
+      });
     }
-    command?.map(co=>{
-      document.getElementById("container-commands").innerHTML +=" <p class='command'>>>"+co+"</p>"
-    })
-    console.log(command)
+    document.getElementById("container-commands").innerHTML = "";
+    command?.map((co) => {
+      document.getElementById("container-commands").innerHTML +=
+        " <p class='command'>>>" + co + "</p>";
+    });
+    document.getElementById("container-commands").innerHTML +=
+      '<p class="command">>><input type="text" id="options" class="input-options" onkeypress="enterCommand(event)"/></p>';
+    console.log(command);
   }
-}
+};
