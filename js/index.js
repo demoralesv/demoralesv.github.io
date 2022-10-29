5bed3ef8d6fcbca75477dd6685e470423dbf93cb
steven15HC <steven99854@gmail.com>
2022-10-28 21:18:35 -0600

    Update index.js

--- js/index.js
+++ js/index.js
@@ -22,7 +22,7 @@ const enterCommand = (event) => {
     command.push(document.getElementById("options").value);
     displayOptiontsMenu(document.getElementById("options").value);
     document.getElementById("options").value = "";
-    if (command?.length == 5) {
+    if (command?.length == 3) {
       command.shift();
     }
     document.getElementById("container-commands").innerHTML = "";
@@ -38,20 +38,20 @@ const enterCommand = (event) => {
 
   if (command?.length > 0) {
     if (event.keyCode === 40) {
-      console.log("baja");
       numberCommand++;
       if (numberCommand > command?.length) {
-        numberCommand = 0;
-        document.getElementById("options").value = "";
+        numberCommand = 1;
+        document.getElementById("options").value = command[numberCommand - 1];
       } else {
         document.getElementById("options").value = command[numberCommand - 1];
       }
     }
 
     if (event.keyCode === 38) {
-      console.log("sube");
       numberCommand--;
       if (numberCommand < 0) {
+        numberCommand = numberCommand - 1;
+        document.getElementById("options").value = command[numberCommand - 1];
         return;
       } else {
         document.getElementById("options").value = command[numberCommand];
@@ -69,14 +69,14 @@ const enterCommand = (event) => {
 */
 const displayOptiontsMenu = (option) => {
   if (page === 0) {
-    if (["A","a"].includes(option)) {
+    if (["A", "a"].includes(option)) {
       document.getElementById("page1").style.display = "none";
       document.getElementById("page2").style.display = "block";
       page = 1;
     }
   }
   if (page === 1) {
-    if (["M","m"].includes(option)) {
+    if (["M", "m"].includes(option)) {
       document.getElementById("page2").style.display = "none";
       document.getElementById("page1").style.display = "block";
       page = 0;
